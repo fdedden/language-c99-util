bbd95a441155d8f0c545b1e1b52401d245922677
Frank Dedden <dev@dedden.net>
2019-01-31 20:29:22 +0100

    Added IsList for TypeQualList and StructDeclnList

---
+++ src/Language/C99/Util/IsList.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Language.C99.Util.IsList where
+
+import GHC.Exts
+
+import Language.C99.AST
+import Language.C99.Util
+
+error_emptylist name = error $ "Empty " ++ name ++ " is not allowed"
+
+instance IsList TypeQualList where
+  type Item TypeQualList = TypeQual
+  fromList []     = error_emptylist "TypeQualList"
+  fromList (x:xs) = foldl TypeQualCons (TypeQualBase x) xs
+  toList = undefined
+
+
+instance IsList StructDeclnList where
+  type Item StructDeclnList = StructDecln
+  fromList []     = error_emptylist "StructDeclnList"
+  fromList (x:xs) = foldl StructDeclnCons (StructDeclnBase x) xs
+  toList = undefined
