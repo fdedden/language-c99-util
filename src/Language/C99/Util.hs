7a81aa4a7dece1efd6f049a79714b57d814e44eb
Frank Dedden <dev@dedden.net>
2019-03-27 17:11:39 +0100

    Added Util.hs file

---
+++ src/Language/C99/Util.hs
@@ -0,0 +1,9 @@
+module Language.C99.Util
+  ( module Language.C99.Util.Wrap
+  , module Language.C99.Util.IsList
+  , module Language.C99.Util.Expr
+  ) where
+
+import Language.C99.Util.Wrap
+import Language.C99.Util.IsList
+import Language.C99.Util.Expr
